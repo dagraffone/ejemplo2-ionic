d0e99bdde1ab81b790f07cf5301e569d6392ba81
dagraffone <diegocifuentes30@gmail.com>
2021-08-20 13:03:28 -0400

    confirmo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  constructor(private router: Router , private navControl: NavController) {}
+  
+  irPagina2(){
+    this.router.navigate(['/pagina2']);
+  }
+  irPagina3(){
+    this.navControl.navigateForward(['/pagina3']);
+  }
 }
